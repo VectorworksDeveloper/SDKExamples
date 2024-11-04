bacd79226d6f0904127aa1288731d10803d03201
Vlad Stanev <vstanev@vectorworks.net>
2024-11-04 15:47:20 -0500

    front-end update for Vectorworks SDK 2025

--- Examples/WebPaletteExample/SourceHTML/src/app.js
+++ Examples/WebPaletteExample/SourceHTML/src/app.js
@@ -5,14 +5,26 @@ import "./app.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Vectorworks localized strings
-exampleIntegrator.getLocalStrings().then( (txt) =>
-{
-	let arr = JSON.parse(txt);
-	document.trans = (str) => {
-		return arr[str] ?? str + '_LOCALIZE';
-	}
-})
+// Vectorworks SDK 2025 will automatically provide dark mode support for your web view
+// via this setup here
+window.setVectorworksThemeCallback((isDark) => {
+    document.body.setAttribute('data-color-scheme', isDark ? 'dark' : 'light');
+});
+
+/* pre Vectorworks SDK 2025 code uses custom getLocalStrings function in our integrator object
+exampleIntegrator.getLocalStrings()
+*/
+//
+// Vectorworks SDK 2025 we use IInitContext::AddReourceAccessFunction in our CPaletteJSProvider::OnInit
+// then we can access any resources from the .vwr file
+exampleIntegrator.getResource('Strings/html.vwstrings')
+.then( (txt) =>
+	{
+		let arr = JSON.parse(txt);
+		document.trans = (str) => {
+			return arr[str] ?? str + '_LOCALIZE';
+		}
+	})
 .catch( (err) => {
 	console.error('Error obtaining localized strings from Vectorworks: ' +err);
 	document.trans = (str) => { return str; }
